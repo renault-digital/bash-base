8fa7b2a55679636713339f0ed950ff1a85f592d9
Hongjie Zhang <hongjie.zhang@renault.com>
2020-09-07 17:50:04 +0200

    ci: add config for @semantic-release/git

--- .releaserc.js
+++ .releaserc.js
@@ -5,7 +5,10 @@ module.exports = {
     "@semantic-release/changelog",
     "@semantic-release/npm",
     "@semantic-release/github",
-    "@semantic-release/git",
+    ["@semantic-release/git", {
+      "assets": ['CHANGELOG.md', 'package.json', 'package-lock.json', 'npm-shrinkwrap.json', 'docs/references.md', 'man/bash-base.1'],
+      "message": "chore(release): ${nextRelease.version} [skip ci]"
+    }],
     ["@semantic-release/exec", {
       "prepareCmd": "docker build -t renaultdigital/bash-base . && npm run livedoc"
     }],
