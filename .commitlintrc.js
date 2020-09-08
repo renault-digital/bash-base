0a2c7c34c1de5e52359f3f426946465f8e84dda7
Hongjie Zhang <hongjie.zhang@renault.com>
2020-09-08 18:03:26 +0200

    ci: use only commitlint for audit commit message
    - config max line length for body&footer to pass the audit of existed
    release commit message - use commitlint to audit all commit messages
    locally
    - no commitsar any more

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,3 +1,11 @@
+const { types } = require('conventional-commit-types');
+
 module.exports = {
-    extends: ['@commitlint/config-conventional']
+    extends: ['@commitlint/config-conventional'], // issuePrefixes: ['#']
+    rules: {
+        // Follow the commit-types used by commitizen (https://git.io/Je4pr).
+        'type-enum': [2, 'always', Object.keys(types)],
+        'body-max-line-length': [2, 'always', 150],
+        'footer-max-line-length': [2, 'always', 200],
+    },
 };
