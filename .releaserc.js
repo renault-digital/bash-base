d0931343be306bc4e96a06b18ac3feaae7b91a46
Hongjie Zhang <hongjie.zhang@renault.com>
2020-09-08 18:06:31 +0200

    ci: improve the config of semantic release
    - correct the order of plugins to generate livedoc before commiting and
    tagging
    - customize the release rule

--- .releaserc.js
+++ .releaserc.js
@@ -1,17 +1,27 @@
 module.exports = {
   "plugins": [
-    "@semantic-release/commit-analyzer",
+    ["@semantic-release/commit-analyzer", {
+        // https://github.com/semantic-release/commit-analyzer#releaserules
+        releaseRules: [
+          { scope: 'no-release', release: false },
+          { breaking: true, release: 'major' },
+          { type: 'feat', release: 'minor' },
+          { type: 'perf', release: 'minor' },
+          { type: 'refactor', release: 'minor' },
+          { type: '*', release: 'patch' },
+        ],
+    }],
     "@semantic-release/release-notes-generator",
     "@semantic-release/changelog",
     "@semantic-release/npm",
-    "@semantic-release/github",
-    ["@semantic-release/git", {
-      "assets": ['CHANGELOG.md', 'package.json', 'package-lock.json', 'npm-shrinkwrap.json', 'docs/references.md', 'man/bash-base.1'],
-      "message": "chore(release): ${nextRelease.version} [skip ci]"
-    }],
     ["@semantic-release/exec", {
       "prepareCmd": "docker build -t renaultdigital/bash-base . && npm run livedoc"
     }],
+    ["@semantic-release/git", {
+      "assets": ['CHANGELOG.md', 'package.json', 'package-lock.json', 'npm-shrinkwrap.json', 'src', 'docs', 'man'],
+      "message": "chore(release): ${nextRelease.version} [skip ci]"
+    }],
+    "@semantic-release/github",
     ["semantic-release-docker", {
       "name": "renaultdigital/bash-base"
     }]
