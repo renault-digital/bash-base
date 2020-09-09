58419694ab82d6e84b90ff0d1a93dced41ba138d
Hongjie Zhang <hongjie.zhang@renault.com>
2020-09-09 08:24:04 +0200

    ci: improve cicd config
    - for commitlint, remove the not useful enum types, because it's the
    default value
    - for semantic-release git pluginn, reuse the default
    message template
    - augument the commitlint length limit to pass the
    audit for commit message including release notes

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,11 +1,7 @@
-const { types } = require('conventional-commit-types');
-
 module.exports = {
     extends: ['@commitlint/config-conventional'], // issuePrefixes: ['#']
     rules: {
-        // Follow the commit-types used by commitizen (https://git.io/Je4pr).
-        'type-enum': [2, 'always', Object.keys(types)],
-        'body-max-line-length': [2, 'always', 150],
-        'footer-max-line-length': [2, 'always', 200],
+        'body-max-line-length': [2, 'always', 300],
+        'footer-max-line-length': [2, 'always', 300],
     },
 };
--- .releaserc.js
+++ .releaserc.js
@@ -19,11 +19,11 @@ module.exports = {
     }],
     ["@semantic-release/git", {
       "assets": ['CHANGELOG.md', 'package.json', 'package-lock.json', 'npm-shrinkwrap.json', 'src', 'docs', 'man'],
-      "message": "chore(release): ${nextRelease.version} [skip ci]"
     }],
     "@semantic-release/github",
     ["semantic-release-docker", {
       "name": "renaultdigital/bash-base"
     }]
-  ]
+  ],
+  "preset": "conventionalcommits"
 }
