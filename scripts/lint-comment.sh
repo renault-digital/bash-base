4160f8fbf32954a9ffeafcbf3ad49dbc58350a2f
Hongjie Zhang <hongjie.zhang@renault.com>
2020-09-10 10:14:32 +0200

    ci: lint comment for the files in src/, not bin/

--- scripts/lint-comment.sh
+++ scripts/lint-comment.sh
@@ -3,11 +3,18 @@
 source src/document.sh
 source src/tool.sh
 
-shellScriptFile="bin/bash-base.sh"
-
 print_header 'format source code'
 npm run shfmt
 
 print_header 'lint script comment'
-doc_lint_script_comment "${shellScriptFile}"
-stop_if_failed 'the comment is not valid'
+for filename in src/*.sh; do
+	doc_lint_script_comment "${filename}" || ((nbFailed++))
+done
+
+print_header 'print global result'
+if [[ "${nbFailed}" -gt 0 ]]; then
+	print_error 'invalid comment existed'
+	exit 1
+else
+	print_success 'all comments are valid'
+fi
