978446784e50d05d1e6c240a60d3d09262aadda9
Hongjie Zhang <hongjie.zhang@renault.com>
2020-09-10 10:49:07 +0200

    refactor: change the color of warning

--- src/constant.sh
+++ src/constant.sh
@@ -3,7 +3,7 @@
 NEW_LINE_SED="\\$(echo -e '\r\n')" # Constant: the return and new line character, used with sed
 COLOR_BOLD_BLACK=$'\e[1;30m'       # Constant: color for printing Header
 COLOR_BOLD_RED=$'\e[1;91m'         # Constant: color for printing message of Error/KO
-COLOR_BOLD_YELLOW=$'\e[1;93m'      # Constant: color for printing message of Warning
+COLOR_BOLD_YELLOW=$'\e[0;33m'      # Constant: color for printing message of Warning
 COLOR_BOLD_GREEN=$'\e[1;32m'       # Constant: color for printing message of OK
 COLOR_BLUE=$'\e[0;34m'             # Constant: color for printing Value
 COLOR_END=$'\e[0m'                 # Constant: color for others, reset to default
