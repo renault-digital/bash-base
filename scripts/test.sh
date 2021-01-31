5d20bc7940ae5319e3ca6e58f6dae16fdb8319d0
Hongjie Zhang <hongjie.zhang@renault.com>
2021-01-31 22:05:01 +0100

    ci: fix the version of shellspec

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,4 +2,4 @@
 
 # use this script to check if bash-base is compatible with your bash and environment
 
-docker run --rm -v "$(pwd)":/bash-base -w /bash-base shellspec/shellspec:master-kcov "$@" spec/*.spec.sh
+docker run --rm -v "$(pwd)":/bash-base -w /bash-base shellspec/shellspec:0.27-kcov "$@" spec/*.spec.sh
